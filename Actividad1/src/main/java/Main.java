7599e7c9df8fa6c8954d379886c1731767267f3b
2dawb <2dawb@informatica.edu@PC234002.informatica.edu>
2025-10-03 10:38:22 +0100

    Preparar bucle + Permitir salir del programa

--- Actividad1/src/main/java/Main.java
+++ Actividad1/src/main/java/Main.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        boolean salir = false;
 
+        do {
+            System.out.println("""
+                    === Gestor de estudiantes ===
+                    1. Añadir estudiante
+                    2. Listar estudiantes
+                    3. Buscar por nombre
+                    4. Calcular nota media general
+                    5. Mostrar mejor estudiante
+                    6. Salir""");
+            System.out.print("Seleccione opción");
+            opcion = sc.nextInt();
+
+            if (opcion == 1) {
+                
+            } else if (opcion == 2) {
+
+            } else if (opcion == 3) {
+
+            } else if (opcion == 4) {
+
+            } else if (opcion == 5) {
+
+            } else if (opcion == 6) {
+                salir = true;
+            }
+            else {
+                System.out.println("Opción no válida. Inténtalo de nuevo");
+            }
+
+        } while (!salir);
     }
 }
